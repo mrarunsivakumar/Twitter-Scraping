e85cb73d91e16f5d26c2663b043e8587868b5c14
mrarunsivakumar <122074011+mrarunsivakumar@users.noreply.github.com>
2023-01-31 12:11:40 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,44 +1,36 @@
 import pandas as pd
 import streamlit as st
-
+import datetime
 
 st.title("TWITTER SCRAPING!")
-
-uploaded_file=st.file_uploader('choose a csv file',type='csv')
-if uploaded_file:
-  st.markdown('---')
-  df=pd.read_csv(uploaded_file)
-  st.dataframe(df)
-
-                     
-
+          
+                
 
 @st.cache
 def get_data():
     return pd.read_csv('twitter_scraping.csv')
 df = get_data()
 
+text = st.text_input("Enter Word to Search")
+if text:
+        From = st.date_input("From Date")
+        if From:
+            To = st.date_input("To Date")
+            if To:
+                maxTweets = st.number_input("Number of Tweets",0)
+                if maxTweets:
+                    submit=st.button('submit')
+                    if submit:
+                       df[df['Username'] == text ]
 
-text = df['Text'].unique()
-text = st.selectbox('Text', text)
 
-
-submit=st.button('submit')
-if submit:
-   df[df['Text'] == text]  
-
-@st.cache
 def convert_df(df):
-    
     return df.to_csv().encode('utf-8')
 
 csv = convert_df(df)
-
 st.download_button(
     label="Download data as CSV",
     data=csv,
     file_name='twitter_scraping.csv',
     mime='text/csv',
 )
-
-
